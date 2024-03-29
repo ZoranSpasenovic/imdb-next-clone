af89f112dd72058820f7c9e8918d32cf5809802f
ZoranSpasenovic <spalespasenovic@gmail.com>
2024-03-29 08:03:11 +0100

    adding loading logic

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center mt-12">
+      <img className="h-52" src="loadingSpinner.svg" alt="" />
+    </div>
+  );
+}
